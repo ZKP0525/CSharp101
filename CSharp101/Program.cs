36eb5ba15faec0c60019e23cde55d59e87106e66
KPZ <KPZ@DESKTOP-GOOLRL6>
2022-10-20 23:39:32 +0800

    添加项目文件。

---
+++ CSharp101/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace CSharp101
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
